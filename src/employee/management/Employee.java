1e7686a129028ce97f5f243f285e68c048573805
doshikyu <yu.dashik@gmail.com>
2024-07-09 20:09:45 +0900

    Basic Level Completed   - Completed the Employee class   - added functions to the Main class  - Started Middle Level tasks

--- src/employee/management/Employee.java
+++ src/employee/management/Employee.java
@@ -1,4 +1,67 @@
 package employee.management;
-public class Employee {
 
+import java.util.Objects;
+
+public class Employee {
+    static int idCnt;
+    private final int id;
+    private String fullName;
+    private int department;
+    private float salary;
+
+    public Employee(String fullName, int department, float salary) {
+        if (fullName.isEmpty() || department < 1 || department > 5 || salary < 0)
+            throw new IllegalArgumentException("Wrong arguments passed to the constructor");
+        this.id = ++idCnt;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        if (fullName.isEmpty())
+            throw new IllegalArgumentException("Cannot set the name to an empty string");
+        this.fullName = fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        if (department < 1 || department > 5)
+            throw new IllegalArgumentException("Department must be an integer 1-5");
+        this.department = department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        if (salary < 0)
+            throw new IllegalArgumentException("Salary cannot be negative. This company does not support slavery");
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object employeeToCheck) {
+        if (this == employeeToCheck) return true;
+        if (employeeToCheck == null || getClass() != employeeToCheck.getClass()) return false;
+        Employee employee = (Employee) employeeToCheck;
+        return id == employee.id && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName);
+    }
 }
--- src/employee/management/Main.java
+++ src/employee/management/Main.java
@@ -2,9 +2,162 @@ package employee.management;
 
 public class Main {
     static Employee[] employees = new Employee[10];
-
     public static void main(String[] args) {
+        employees[0] = new Employee("Анна Сергеевна Каренина", 1, 33000);
+        employees[1] = new Employee("Дмитрий Дмитриевич Гуров", 1, 82000);
+        employees[2] = new Employee("Иван Иванович Чимша-Гималайский", 2, 62000);
+        employees[3] = new Employee("Дмитрий Ионыч Старцев", 2, 64000);
+        employees[4] = new Employee("Сергей Сергеевич Толстой", 3, 87000);
+        employees[5] = new Employee("Михаил Васильевич Тонкий", 3, 38000);
+        employees[6] = new Employee("Иван Дмитриевич Лаевский", 4, 57000);
+        employees[7] = new Employee("Надежда Федоровна Михайлова", 4, 37000);
+        employees[8] = new Employee("Андрей Ефимыч Рагин", 5, 97000);
+        employees[9] = new Employee("Николай Васильевич Подгорин", 5, 72000);
+
+        System.out.println("Информация о сотрудниках:\n" + toString(employees));
+        System.out.println("Сумма затрат на ЗП в месяц: " + calculateTotalSalaries());
+        System.out.println("Сотрудник с минимальной ЗП: " + findMinSalaryEmployee().getSalary());
+        System.out.println("Сотрудник с максимальной ЗП: " + findMaxSalaryEmployee().getSalary());
+        System.out.println("Среднее значение зарплат: " + calculateAverageSalary());
+        System.out.println();
+        printEmployeesFullNames();
+        increaseEmployeesWages(4.6F);
+        System.out.println("Сотрудник с минимальной ЗП: " + findMinSalaryEmployee().getSalary());
+        System.out.println("Сотрудник с максимальной ЗП: " + findMaxSalaryEmployee().getSalary());
 
     }
+    public static String toString(Employee[] employees) {
+        StringBuilder employeesStr = new StringBuilder();
+        for (Employee employee : employees) {
+            if (employee != null) {
+                employeesStr.append(employee.getId()).append(",")
+                        .append(employee.getFullName()).append(",")
+                        .append(employee.getDepartment()).append(",")
+                        .append(employee.getSalary()).append("\n");
+            }
+        }
+        return "ID,ФИО,Департамент,Зарплата\n" + employeesStr;
+    }
+
+    public static float calculateTotalSalaries() {
+        float totalSalaries = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                totalSalaries += employee.getSalary();
+            }
+        }
+        return totalSalaries;
+    }
+
+    public static Employee findMinSalaryEmployee() {
+        if (employees == null || employees.length == 0) {
+            throw new IllegalArgumentException("The employees array is null or empty.");
+        }
+        Employee minSalaryEmployee = null;
+        for (Employee employee : employees) {
+            if (employee != null && (minSalaryEmployee == null || employee.getSalary() < minSalaryEmployee.getSalary()))
+                minSalaryEmployee = employee;
+        }
+        if (minSalaryEmployee == null) throw new IllegalArgumentException("All employees in the array are null.");
+        return minSalaryEmployee;
+    }
+
+    public static Employee findMaxSalaryEmployee() {
+        if (employees == null || employees.length == 0) {
+            throw new IllegalArgumentException("The employees array is null or empty.");
+        }
+        Employee maxSalaryEmployee = null;
+        for (Employee employee : employees) {
+            if (employee != null && (maxSalaryEmployee == null || employee.getSalary() > maxSalaryEmployee.getSalary()))
+                maxSalaryEmployee = employee;
+        }
+        if (maxSalaryEmployee == null) throw new IllegalArgumentException("All employees in the array are null.");
+        return maxSalaryEmployee;
+    }
+
+    public static float calculateAverageSalary() {
+        float totalSalaries = calculateTotalSalaries();
+        return totalSalaries / employees.length;
+    }
+
+    public static void printEmployeesFullNames() {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee.getFullName());
+            }
+        }
+    }
+
+    public static void increaseEmployeesWages(float raiseRate) {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                employee.setSalary(employee.getSalary() * (1 + raiseRate / 100));
+            }
+        }
+    }
+
+    /* Methods by Departments */
+    public static Employee[] getDepartmentEmployees (int department) {
+        if (employees == null || employees.length == 0) {
+            return null;
+        }
+        Employee[] deptEmployeesTmp = new Employee[10];
+        int cnt = 0;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getDepartment() == department) {
+                deptEmployeesTmp[cnt++] = employee;
+            }
+        }
+        if (cnt > 0) {
+            Employee[] deptEmployees = new Employee[cnt];
+            System.arraycopy(deptEmployeesTmp, 0, deptEmployees, 0, cnt);
+
+            return deptEmployees;
+        }
+        else return null;
+    }
+
+    public static Employee findMinSalaryEmployeeByDept(int department) {
+        Employee[] deptEmployees = getDepartmentEmployees(department);
+        if (deptEmployees == null || deptEmployees.length == 0) {
+            throw new IllegalArgumentException("The employees array is null or empty.");
+        }
+        Employee minSalaryEmployee = null;
+        for (Employee employee : deptEmployees) {
+            if (employee != null && (minSalaryEmployee == null || employee.getSalary() < minSalaryEmployee.getSalary()))
+                minSalaryEmployee = employee;
+        }
+        if (minSalaryEmployee == null) throw new IllegalArgumentException("All employees in the array are null.");
+        return minSalaryEmployee;
+    }
+
+    public static Employee findMaxSalaryByDept(int department) {
+        Employee[] deptEmployees = getDepartmentEmployees(department);
+        if (deptEmployees == null || deptEmployees.length == 0) {
+            throw new IllegalArgumentException("The employees array is null or empty.");
+        }
+        Employee maxSalaryEmployee = null;
+        for (Employee employee : deptEmployees) {
+            if (employee != null && (maxSalaryEmployee == null || employee.getSalary() > maxSalaryEmployee.getSalary()))
+                maxSalaryEmployee = employee;
+        }
+        if (maxSalaryEmployee == null) throw new IllegalArgumentException("All employees in the array are null.");
+        return maxSalaryEmployee;
+    }
+
+    public static float calculateTotalSalariesInDept(int department) {
+        Employee[] deptEmployees = getDepartmentEmployees(department);
+        if (deptEmployees == null || deptEmployees.length == 0) {
+            throw new IllegalArgumentException("The employees array is null or empty.");
+        }
+        float totalSalaries = 0;
+        for (Employee employee : deptEmployees) {
+            if (employee != null) {
+                totalSalaries += employee.getSalary();
+            }
+        }
+        return totalSalaries;
+    }
+
 
 }
